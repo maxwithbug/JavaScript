a67b61694805f6a4bda66f73826f5b7f52e81d31
suman <sumanpakira86@gmail.com>
2023-09-22 23:34:24 +0530

    done 2 tasks

--- WEEK13/Task1.js
+++ WEEK13/Task1.js
@@ -1,7 +1,48 @@
+
+
+
+//TASK 1 :
 //this code is foor calculating discount in total amount
 
+let totalvalue = 1000;
+let discount = 20 ; 
 
-let totalvalue = 20000;
+let afterdiscount = totalvalue-(totalvalue*discount/100);
+console.log(afterdiscount);
+
+
+// TASK 2 : 
+// This code is for cheacking conditions for login page
+
+
+let username = "admin";
+let password = "12345"
+
+if (username="admin" && password == "12345"){
+    console.log("login successfull");
+}else{
+    console.log("login failed");
+}
+
+//TASK 3 :
+//this code for converting (INR) TO (USD) 
+
+
+let INR = 82 ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
