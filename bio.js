7cbfe7325d9990a19642b8c238c2380d2b7dc3a0
MAxX <121168928+maxwithbug@users.noreply.github.com>
2024-01-19 03:36:42 +0530

    Delete bio.js

--- bio.js
+++
@@ -1,9 +0,0 @@
-const i = {}
-i.am = 'A fullstack developer'
-i.workwith = ['html','css','React','tailwindCss','node.js','express.js','mongoDB','git','SQl','google cloud']
-i.exprienced = ['MERN internship from topstack']
-i.aspire = 'to provide free learning and want to inspire to solve real world problem '
-
-
-
-console.log(i);
