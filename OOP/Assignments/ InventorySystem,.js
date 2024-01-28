a3dcf497285232e4cfc327308a9ee90b36892878
suman <sumanpakira86@gmail.com>
2024-01-29 02:35:14 +0530

    fixed task 5

--- OOP/Assignments/ InventorySystem,.js
+++ OOP/Assignments/ InventorySystem,.js
@@ -5,33 +5,40 @@ function Product(name, category, price,stock){
     this.stock = stock
 }
 //making an Inventory constructor function
-function Inventory(product){
+function Inventory(){
     this.item = []
-
-    this.addproduct = (product)=>{
-        this.item.push(product)
-    }
-
-    this.deleteproduct = (product)=>{
-        // let deleteitem = this.item.filter((itm)=>itm.name===product)  
-        const remainingproductARRAY  =  this.item.filter((itm)=>itm!=product)  //The (filter) method returns a new array containing the elements that pass the provided function's test .and storing new input again in array  
-        this.item = remainingproductARRAY;
-        if(remainingproductARRAY.length>0){
-            console.log(`product ${product} deleted sucessfully`);
-            console.log(remainingproductARRAY);
-        }else{
-            console.log(`wrong input for delete`);
-        }
-    }
 }
+
+
+// Inventory.prototype.addproduct = ((product)=>{     
+//     this.item.push(product)
+// })
+
+Inventory.prototype.addproduct = function(product){     //don't use Arrow function ,(this keyword) won't work
+    this.item.push(product)
+}
+Inventory.prototype.deleteproduct = function(productName){    //don't use Arrow function ,(this keyword) won't work
+    const itemindex = this.item.findIndex((product)=>product.name===productName)
+    if(itemindex!==-1){
+        this.item.splice(itemindex,1)
+        console.log(`product ${productName} deleted sucessfully`);
+    }else{
+        console.log(`wrong input for delete`);
+    }
+    console.log(`remaining products are:`);
+    console.log(this.item);
+}
+
 
 const inventory = new Inventory
 
 const product1 = new Product('laptop','electronics',70000,100)
 const product2 = new Product('Book','Book',300,10)
+const product3 = new Product('jj','nob',9900,10)
 
 inventory.addproduct(product1)
 inventory.addproduct(product2)
+inventory.addproduct(product3)
 console.log(`products, added sucessfully`);
 console.log(inventory.item);
 
