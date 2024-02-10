0fae3f1d3bd16cbb15676741f73630a93e2c90b7
suman <sumanpakira86@gmail.com>
2024-02-11 01:47:08 +0530

    startig destructure and closure

---
+++ DestructuringAndClosure/SpradeAndRestOparetor.js
@@ -0,0 +1,83 @@
+/*sprade and rest oparetor */
+//*** they both are denoted by[...] */
+
+
+/*WORK*/
+/*SPRADE oparetor*/
+// 1.can create new copy
+// 2.adding new values
+// 3.concatenate two arrays
+// 4.sprade an Array of arguments as Individuals
+// 5.can be used with string , objects 
+// 6.sprade Converted the iterable Into single arguments....
+
+/*1st ex... */
+let arr1 = [13,45,88,90,0] 
+let arr2 = [...arr1]  //works only SHALLOW copy 
+console.log('arr2',arr2); 
+/*2st ex... */
+let arr3 = [13,45,88] 
+let arr4 = [...arr3,90,0]  //adding values
+console.log('arr4',arr4); 
+/*3st ex... */
+
+/*4st ex... */
+/*5st ex... */
+/*6st ex... */
+
+
+
+
+
+
+
+/*REST oparetor*/
+//rest Converted single elements into an array...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
